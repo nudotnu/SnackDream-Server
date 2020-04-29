25adabb1e645989f00ad933902939c818605a450
Areum1 <s2018s10@e-mirim.hs.kr>
2020-04-29 21:53:43 +0900

    파이썬 서버 완성

--- flask/calorie.py
+++ flask/calorie.py
@@ -3,28 +3,30 @@ import json # import json module
 import random
 app = Flask(__name__)
 
-print("hello")
-
 @app.route('/snack', methods=['GET', 'POST'])
-
 def snacks():
-    
-    with open('config.json', encoding='UTF-8') as json_file:
+    file_path = "config.json" 
+    json_new = dict()
+    json_data = {}
+    with open(file_path, encoding='UTF-8') as json_file:
         json_data = json.load(json_file)
         json_string = json_data["action"]["parameters"]["shortcut"]["value"]
         print(json_string)
-        nugu_output = json_data["output"]
-        nugu_output = "hello"
 
-        nugu_prom = {
-            'output': {
-                'prompt': nugu_output
-            }
-        }
+    json_new = {
+        "prompt": json_string
+    }
 
-        print(nugu_prom)
-        return nugu_prom
-    
+    json_data["output"] = json_new
+
+    with open(file_path, 'w', encoding='utf-8') as make_file:
+        json.dump(json_data, make_file, indent="\t")
+
+    # with open(file_path, 'r', encoding='utf-8') as f:
+    #     json_data = json.load(f)
+    # print(json.dumps(json_data, indent="\t") )
+
+    return jsonify(json_data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port=5000, threaded=True)
